a8874e382d9241521bd4e40ec1f1749ccad07508
orini <ori.nizri.tal@gmail.com>
2021-11-27 23:41:11 +0200

    16th commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,17 @@
-let test = document.querySelector(".cart-count"); // default textCeontent is 0
+let test = document.querySelector(".cart-count"); // Default textCeontent is 0
 const addCartButton = document.querySelector(".add-cart");
 const removeCartButton = document.querySelector(".remove-cart");
-const quantity = document.querySelector(".quantity select");
+const quantityButton = document.querySelector(".quantityButton select");
 
+// Bonus! Use the Quantity button to add also 2 and 3 to the cart
 addCartButton.addEventListener('click', () => {
-    test.textContent = parseInt(test.textContent) + parseInt(quantity.value);
+    test.textContent = parseInt(test.textContent) + parseInt(quantityButton.value);
 })
 removeCartButton.addEventListener('click', () => {
-    test.textContent = parseInt(test.textContent) - parseInt(quantity.value);
+    test.textContent = parseInt(test.textContent) - parseInt(quantityButton.value);
 })
 
+// Building objects through class
 class Items {
     constructor(image, name, rating, feedbackNum, price) {
         this.image = image,
@@ -18,9 +20,8 @@ class Items {
         this.feedbackNum = feedbackNum,
         this.price = price
     }
-    
 }
-
+// Construction of new items
 const Item1 = new Items("\\assets\\images\\HelloIMG1637838582380.jpeg", "Cat Toy Laser Pointers, 3 Pack Laser Pointer Cat Toys for Indoor Cats Interactive P...", 3.0, "\(179\)", 'ILS 81.99');
 const Item2 = new Items("\\assets\\images\\HelloIMG1637838560379.jpeg", "NPET Interactive Cat Toy, 360 Degree Rotating Automatic Toys for Indoor Cast, Elect..", 3.5, "\(14\)", 'ILS 68.16');
 const Item3 = new Items("\\assets\\images\\HelloIMG1637838560033.jpeg", "3 Pack Pointer for Cats Dogs Pet Interactive Toys, (Green, Red, Violet) Cat Chase E...", 4.0, "\(44\)", 'ILS 38.29');
@@ -34,12 +35,11 @@ const Item10 = new Items("\\assets\\images\\HelloIMG1637838564333.jpeg", "BENTOP
 const Item11 = new Items("\\assets\\images\\HelloIMG1637838564333.jpeg", "BENTOPAL Laser Cat Toys for Indoor Cats/Dogs, Interactive Cat Toys with Motion...", 4.5, "\(979\)", '$99.99');
 const Item12 = new Items("\\assets\\images\\HelloIMG1637838564333.jpeg", "BENTOPAL Laser Cat Toys for Indoor Cats/Dogs, Interactive Cat Toys with Motion...", 4.5, "\(979\)", '$99.99');
 
-
-const names = document.querySelectorAll(".name a")
+// Location of the items in the html document
 const items_location = document.querySelectorAll(".items")
-
+// Construction of items in array for looping (I would add it into the constractor if I had more time)
 let itemInstances = [Item1, Item2, Item3, Item4, Item5, Item6,Item7, Item8, Item9, Item10, Item11, Item12];
-
+// A Function that assigns the items to the HTML using html_location, items array and index
 function insertAll(location, items, index) {
     for (let i=index, j=0 ; j<location.length ; i++, j++) {
         items_location[j].children[0].firstElementChild.src = itemInstances[i].image
@@ -50,10 +50,11 @@ function insertAll(location, items, index) {
     }
     return index;
 }
+// Setting starting index at 0
 let index = 0;
 insertAll(items_location, itemInstances,index);
-
-let MoreItemsBtn = document.querySelector(".more-items");
+// A function that adds to the carousel the next six items using items' index by clicking on more-items button
+let MoreItemsBtn = document.querySelector(".more-items"); // Next items button location in HTML
 MoreItemsBtn.addEventListener("click", function() {
     console.log("click");
     index += 6;
@@ -62,8 +63,8 @@ MoreItemsBtn.addEventListener("click", function() {
     console.log("arrived");
     return index;
 })
-
-let PreviousItemsBtn = document.querySelector(".previous-items");
+// A function that adds to the carousel the six previous items using items' index by clicking on the previous items button
+let PreviousItemsBtn = document.querySelector(".previous-items"); // Previous items button location in HTML
 PreviousItemsBtn.addEventListener("click", function() {
     console.log("click");
     index -= 6;
