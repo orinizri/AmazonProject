6d257c4ec186dc8ca44a0a9337d4eb4ddd4b61cc
orini <ori.nizri.tal@gmail.com>
2021-11-27 14:54:00 +0200

    11th commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,30 +3,34 @@ const addCartButton = document.querySelector(".add-cart");
 const removeCartButton = document.querySelector(".remove-cart");
 const quantity = document.querySelector(".quantity select");
 
-addCartButton.addEventListener ('click' , () => {
+addCartButton.addEventListener('click', () => {
     test.textContent = parseInt(test.textContent) + parseInt(quantity.value);
 })
-removeCartButton.addEventListener ('click' , () => {
+removeCartButton.addEventListener('click', () => {
     test.textContent = parseInt(test.textContent) - parseInt(quantity.value);
 })
 
 class Items {
-    constructor(name,rating,feedbackNum, price) {
-    this.name = name,
-    this.rating =rating,
-    this.feedbackNum=feedbackNum,
-    this.price = price
+    constructor(image, name, rating, feedbackNum, price) {
+        this.image = image,
+            this.name = name,
+            this.rating = rating,
+            this.feedbackNum = feedbackNum,
+            this.price = price
     }
 }
-const Item1 = new Items("Cat Toy Laser Pointers, 3 Pack Laser Pointer Cat Toys for Indoor Cats Interactive P...",4.5,179,'$16.99');
+const Item1 = new Items("\\assets\\images\\HelloIMG1637838582380.jpeg","Cat Toy Laser Pointers, 3 Pack Laser Pointer Cat Toys for Indoor Cats Interactive P...", 4.5, 179, '$16.99');
 //console.log(Item1);
-const carousel = document.querySelector("#name");
-console.log(carousel.innerHTML)
+const carousel = document.querySelectorAll(".image img");
+console.log(carousel);
 
-function insertItem (location, item) {
-    console.log(`location.innerHTML is getting replaced`);
-        location.innerHTML = item.name;
-        console.log(`${location.innerHTML}`)
+function insertItems(location, item) {
+    for (let i=0 ; i<location.length ; i++) {
+        console.log(i)
+        location[i].src = item.image;
+    }
+    location.src = item.image;
+    console.log(`${location.src}`)
 }
 
-console.log(insertItem(carousel, Item1))
+console.log(insertItems(carousel, Item1))
